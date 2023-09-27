974db441b3202ed13d11e4114dd85d3d82fb96cb
pc-tholc\tholc <tholc@outlook.com>
2023-09-27 17:49:15 +0700

    publish demo event and delegate

---
+++ EventsAndDelegates/EventsAndDelegates/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new Video() { Title = "Video 1"};
+            var videoEncoder = new VideoEncoder(); // publisher
+            var mailService = new MailService(); // subcriber
+            var messageService = new MessageService(); // subcriber
+
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
+            videoEncoder.VideoEncoded += messageService.OnVideoEncoded;
+
+            videoEncoder.Encode(video);
+        }
+
+    }
+
+    
+}
